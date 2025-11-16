6a4593b6195588e93e789bb972ad1c7a3f075d2e
Jahir <kjahir@yahoo.com>
2025-11-16 17:21:52 +0800

    Quick deploy

--- setup-secrets.sh
+++ setup-secrets.sh
@@ -2,3 +2,4 @@ RENDER_DEPLOY_HOOK_BACKEND=https://api.render.com/deploy/srv-d4cm20qli9vc73c3072
 RENDER_BACKEND_URL=https://seyali-test.onrender.com/
 VERCEL_TOKEN=MjS4IyC7FzZYobD4k8pWBu2Z
 VERCEL_PROJECT_ID=prj_zCSSxIxPzMuXRcdHdckhK0yizVHe
+#remove
