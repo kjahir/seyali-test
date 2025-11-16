12a1034d2971c1dc0194482fa63f50df626eed20
Jahir <kjahir@yahoo.com>
2025-11-16 16:08:14 +0800

    Production ready!

---
+++ setup-secrets.sh
@@ -0,0 +1,4 @@
+RENDER_DEPLOY_HOOK_BACKEND=https://api.render.com/deploy/srv-d4cm20qli9vc73c3072g?key=RP1KhQNOA_g
+RENDER_BACKEND_URL=https://seyali-test.onrender.com/
+VERCEL_TOKEN=MjS4IyC7FzZYobD4k8pWBu2Z
+VERCEL_PROJECT_ID=prj_zCSSxIxPzMuXRcdHdckhK0yizVHe
